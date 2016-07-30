6332319c35601d0ae553c081f3ff6bad491cdcf5
Balaji Baskran <bbala.ji20@gmail.com>
2016-07-29 23:26:50 -0400

    refactored with _

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 
+var _ = require('underscore');
+
 var PORT = process.env.PORT || 3000;
 var todos = [];
 var todonextId = 1;
@@ -15,14 +17,25 @@ app.get('/', function (req, res) {
 app.get('/todos', function (req, res) {
     res.json(todos);
 });
+
+
+/*
+ * refactor with _ as it has many in built functions like for searching, etc, eg., where(, findWhere() function
+ */
+
 app.get('/todos/:id', function (req, res) {
     var todoid = parseInt(req.params.id,10);
-    var found;
-    todos.forEach(function (todo) {
+    
+    //findwhere returns one match!
+    var found = _.findWhere(todos, {id:todoid});
+    
+    /*todos.forEach(function (todo) {
         if (todo.id === todoid) {
             found = todo;
         }
-    });
+    });*/
+    
+    
     if (!found) {
         res.status(404).send();
     } else {
@@ -32,7 +45,19 @@ app.get('/todos/:id', function (req, res) {
 
 app.post('/todos', function(req, res){
     var body = req.body;
-    //console.log('description..');
+        
+    body = _.pick(req.body, 'description', 'completed');
+    //console.log(cleanedTodo);
+    /*
+     * validate for correct properties, and make sure no additional prop is given in
+     */
+    if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length == 0 ){
+        return res.status(400).send();
+        //400 bad data
+    }
+    
+    body.description = body.description.trim();
+    
     body.id = todonextId++;
     todos.push(body);
     res.json(body);
