00ea48da4e3a9dc948e56e935f96077b46adbaf6
Balaji Baskran <bbala.ji20@gmail.com>
2016-07-29 21:41:46 -0400

    todos and todos:id

--- server.js
+++ server.js
@@ -1,22 +1,39 @@
 var express = require('express');
-
 var app = express();
-
 var PORT = process.env.PORT || 3000;
-
-app.get('/', function(req, res){
+var todos = [{
+    id: 1
+    , description: 'Talking to my mom'
+    , completed: false
+}, {
+    id: 2
+    , description: 'Have dinner'
+    , completed: false
+}, {
+    id: 3
+    , description: 'Have fun'
+    , completed: true
+}];
+app.get('/', function (req, res) {
     res.send('todo API root');
 });
-
-
-
-
-
-
-
-
-
-
-app.listen(PORT, function(){
-   console.log('server started at '+PORT+'....');
+app.get('/todos', function (req, res) {
+    res.json(todos);
+});
+app.get('/todos/:id', function (req, res) {
+    var todoid = parseInt(req.params.id,10);
+    var found;
+    todos.forEach(function (todo) {
+        if (todo.id === todoid) {
+            found = todo;
+        }
+    });
+    if (!found) {
+        res.status(404).send();
+    } else {
+        res.json(found);
+    }
+});
+app.listen(PORT, function () {
+    console.log('server started at ' + PORT + '....');
 })
